4fd7310345d604847c0bcab6d7f66fccce431852
trung142p <trung142p@gmail.com>
2026-03-30 20:25:55 +0700

    thay đổi email cloudinary và render.

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name: "ddivnd5nh",
-    api_key: "173314725861216",
-    api_secret: "ty0fM7sqymYkJOVj-u51IDcm9rQ"
+    cloud_name: "dxma579lf",
+    api_key: "162493944154219",
+    api_secret: "yluFNY3xWntrS1mIfjd-O-KT2hc"
 });
 
 module.exports = cloudinary;
